57ffadfa48375b3155789667be4734a233e3a479
EmilioCliff <emiliocliff@gmail.com>
2023-05-15 16:37:46 +0300

    first upload

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+arguments = argv
+length = len(arguments) - 1
+if length == 1:
+    print("{} argument:".format(length))
+    print("{}: {}".format(length, arguments[length]))
+elif length == 0:
+    print("{} arguments:".format(length))
+else:
+    print("{} arguments:".format(length))
+    for c in range(length):
+        print("{}: {}".format(c, arguments[c + 1])) 
