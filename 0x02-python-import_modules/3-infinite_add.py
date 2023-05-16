d714107d01b758efa7cb2c5faff2381b888146e6
EmilioCliff <emiliocliff@gmail.com>
2023-05-16 12:01:44 +0300

    Final fixes

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+    arguments = (sys.argv)
+    length = len(arguments) - 1
+    addition = 0
+    for count in range(length):
+        addition = addition +  int(arguments[count + 1])
+    print("{}".format(addition))
