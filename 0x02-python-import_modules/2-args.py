639ced7b4e6e3e99266c3771e79cecc07249c982
EmilioCliff <emiliocliff@gmail.com>
2023-05-17 08:26:26 +0300

    fixed according to pycodestle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-
-from sys import argv
-arguments = argv
-length = len(arguments) - 1
-if length == 1:
-    print("{} argument:".format(length))
-    print("{}: {}".format(length, arguments[length]))
-elif length == 0:
-    print("{} arguments:".format(length))
-else:
-    print("{} arguments:".format(length))
-    for c in range(length):
-        print("{}: {}".format(c, arguments[c + 1])) 
+    from sys import argv
+    arguments = argv
+    length = len(arguments) - 1
+    if length == 1:
+        print("{} argument:".format(length))
+        print("{}: {}".format(length, arguments[length]))
+    elif length == 0:
+        print("{} arguments:".format(length))
+    else:
+        print("{} arguments:".format(length))
+        for c in range(length):
+            print("{}: {}".format(c, arguments[c + 1]))
