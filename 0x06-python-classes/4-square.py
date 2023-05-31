0f2c7e5494b8a4d6f01d5e9705c4b429a3f9e28d
EmilioCliff <emiliocliff@gmail.com>
2023-05-31 15:52:57 +0300

    added getter and setter methods

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,7 +3,6 @@
 
 
 class Square:
-    """initialized the square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -11,6 +10,19 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    """returns the area of the square"""
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
     def area(self):
         return self.__size ** 2
