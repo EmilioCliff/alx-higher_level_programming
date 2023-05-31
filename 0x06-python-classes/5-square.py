a39e242bc0f9b80d79a21438f4f8cb35848b5fff
EmilioCliff <emiliocliff@gmail.com>
2023-05-31 16:25:10 +0300

    prints the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -11,11 +11,13 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     """gets the value of size"""
     @property
     def size(self):
         return self.__size
     """sets the size to value"""
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -24,6 +26,15 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     """returns the area of the square"""
     def area(self):
         return self.__size ** 2
+
+    """prints # in stdout and empty is size == 0"""
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                print("#" * self.__size)
