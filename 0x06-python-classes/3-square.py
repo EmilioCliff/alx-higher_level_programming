db11b5392bf7eb7343d700b6c9436da70fd0d661
EmilioCliff <emiliocliff@gmail.com>
2023-05-31 15:20:09 +0300

    Added an instance method for calculating the area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,11 +3,14 @@
 
 
 class Square:
-    """initialize the square to specific objects"""
+    """initialized the square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif size <= 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+    """returns the area of the square"""
+    def area(self):
+        return self.__size ** 2
